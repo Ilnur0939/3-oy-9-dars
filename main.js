40d653763c15a1a2db90b118d90559e8db06cf32
Пользователь <Ilnur0939>
2022-11-25 15:44:21 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+let form = document.querySelector('#form')
+let text = document.querySelector('#text')
+
+form.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    let inp = document.querySelector('#inp').value;
+    let n = 0
+    for(let i = 0; i < inp.length; i++){
+        let a = /[a,u,e,i,o,o',A,U,E,I,O,O']/
+        if(a.test(inp[i])){
+            // n = n++
+            text.innerHTML = `${n += 1 } ta unli harf bor`
+        }
+        else{
+            text.textContent = n
+        }
+    }
+    console.log(n);
+})
